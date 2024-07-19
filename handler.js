78f80d9a9c3bde0d95029fd8d7355ec223045ad5
Allen Chun <allen_oliver_marcos_chun_from.scientec@stb.gov.sg>
2024-07-19 17:28:53 +0800

    Initial Commit

---
+++ handler.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports.list = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+
+module.exports.generate = async (event) => {
+  console.log(event)
+}
