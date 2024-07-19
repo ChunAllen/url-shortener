fcf6fe6afc670eded580c0db22c77cdf7defda03
Allen Chun <allen_oliver_marcos_chun_from.scientec@stb.gov.sg>
2024-07-19 20:37:12 +0800

    Added redirect function

--- handler.js
+++ handler.js
@@ -5,30 +5,70 @@ const uuidv4 = require('uuid/v4')
 const helper = require('./helper')
 
 module.exports.list = async (event) => {
-  let urls = await Url.scan().exec()
+  try {
+    let urls = await Url.scan().exec()
 
-  console.log(urls)
+    return helper.respondCollection(urls)
+  } catch (error) {
+    return helper.genericErrorMessage(error.message)
+  }
 };
 
 
 module.exports.generate = async (event) => {
-  const body = JSON.parse(event.body)
+  try {
+    const body = JSON.parse(event.body)
 
-  const originalUrl = body.original_url
-  const id = uuidv4()
+    const originalUrl = body.original_url
+    const id = uuidv4()
 
-  const params = {
-    id: id,
-    original_url: originalUrl,
-    shortened_url: `${process.env.BASE_URL}/${process.env.STAGE}/s-link/${id}`,
-    createdAt: helper.timestamp(),
-    updatedAt: helper.timestamp()
+    const params = {
+      id: id,
+      original_url: originalUrl,
+      shortened_url: `${process.env.BASE_URL}/s/${id}`,
+      createdAt: helper.timestamp(),
+      updatedAt: helper.timestamp()
+    }
+
+    const url = new Url(params)
+
+    await url.save()
+
+    return helper.respondObject(params)
+  } catch (error) {
+    return helper.genericErrorMessage(error.message)
   }
-
-  console.log(params)
 }
 
 module.exports.redirect = async (event) => {
+  try {
+    const id = (event.pathParameters) ? event.pathParameters.id : event.path.id
 
+    let url = await Url.query({
+      id: id
+    }, {
+      condition: 'attribute_exists(id)'
+    }).exec()
+
+    if (url[0]) {
+      return helper.respondObject(url[0].original_url)
+    } else {
+      return helper.respondNotFound()
+    }
+  } catch (error) {
+    return helper.genericErrorMessage(error.message)
+  }
 }
 
+module.exports.genericErrorMessage = (message) => {
+  return {
+    statusCode: 422,
+    body: JSON.stringify({
+      status: {
+        'code': 422,
+        'errorDetail': message,
+        'message': 'UNPROCESSABLE ENTITY'
+      }
+    })
+  };
+}
--- helper.js
+++ helper.js
@@ -3,3 +3,51 @@ const moment = require('moment-timezone')
 module.exports.timestamp = () => {
   return Number(moment.tz(new Date(), "Asia/Singapore"))
 }
+
+module.exports.respondCollection = (objects) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: {
+        results: objects
+      },
+      status: {
+        'code': 200,
+        'errorDetail': '',
+        'message': 'OK',
+      }
+    })
+  }
+}
+
+module.exports.respondObject = (object) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: {
+        result: object
+      },
+      status: {
+        'code': 200,
+        'errorDetail': '',
+        'message': 'OK',
+      }
+    })
+  }
+}
+
+module.exports.respondNotFound = () => {
+  return {
+    statusCode: 404,
+    body: JSON.stringify({
+      data: {
+        message: "Record Not Found"
+      },
+      status: {
+        'code': 404,
+        'errorDetail': '',
+        'message': 'OK',
+      }
+    })
+  }
+}
